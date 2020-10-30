db44dd77a0835a2597c4aed4731254777dcf48dc
Ailin Chua <ai-chua@outlook.com>
2020-10-30 14:42:23 +0800

    push

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ require('dotenv').config();
 const express = require('express')
 const hbs = require('express-handlebars')
 const bodyParser = require('body-parser')
+const fetch = require('node-fetch')
+const withQuery = require('with-query').default
+
+// USE X-Api-Key HTTP header to hide API KEY
 
 // Configure the environment
 const PORT = parseInt(process.env.PORT) || 3000
@@ -21,12 +25,49 @@ app.set('views', __dirname + '/views')
 app.use(express.static(__dirname + '/static'))
 
 // Prefix match
-app.get(['/', '/index.html'], (req, res) => {
+app.get('/search', async (req, res) => {
+  console.log('--- New Req ---')
+  console.log('search keyword: ', req.query.keyword)
+  console.log('search region: ', req.query.country)
+  console.log('search category: ', req.query.category)
+  const url = withQuery(
+    process.env.ENDPOINT,
+    {
+      apiKey: process.env.API_KEY,
+      q: req.query.keyword,
+      country: req.query.country,
+      category: req.query.category
+    })
+  console.log('search url: ', url)
+  const results = await fetch(url)
+  const headlines = await results.json()
+  if (headlines.totalResults > 0) {
+    console.log(headlines.articles)
+    res.render('index', {
+      articleArr: headlines.articles,
+      searchKeyword: req.query.keyword,
+      searchRegion: req.query.country,
+      searchCategory: req.query.category
+    })
+  } else {
+    console.log('--- NO RESULTS ---')
+    console.log(' ')
+    const searchParams = {
+      searchKeyword: req.query.keyword,
+      searchRegion: req.query.country,
+      searchCategory: req.query.category
+    }
+    res.render('index', { searchParams })
+  }
+})
+
+app.get('/', (req, res) => {
+  console.log('--- HOME NO REQ ---')
+  console.log(' ')
   res.status(200)
   res.type('text/html')
   res.render('index')
 })
-
 
 
 
